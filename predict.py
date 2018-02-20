5e5fa4171fbc32221d803fbb91787ade3f88e914
suyash0103 <suyashghuge@gmail.com>
2018-02-20 21:16:09 +0530

    Added python namedtuple

--- predict.py
+++ predict.py
@@ -8,3 +8,7 @@ import matplotlib.pyplot as plt
 API_KEY = "ef504af496da69c7"
 BASE_URL = "http://api.wunderground.com/api/API_KEY/history_20180221/q/NE/Lincoln.json"
 
+target_date = datetime(2016, 5, 16)
+features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",
+            "mintempm", "maxdewptm", "mindewptm", "maxpressurem", "minpressurem", "precipm"]
+DailySummary = namedtuple("DailySummary", features)
