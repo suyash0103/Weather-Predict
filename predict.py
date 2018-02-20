fe5142ebc13d76c04e27badc2a6e9ff096ca9ab0
suyash0103 <suyashghuge@gmail.com>
2018-02-20 21:12:39 +0530

    Added API Key

--- predict.py
+++ predict.py
@@ -5,4 +5,6 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-print ("Hello")
+API_KEY = "ef504af496da69c7"
+BASE_URL = "http://api.wunderground.com/api/API_KEY/history_20180221/q/NE/Lincoln.json"
+
