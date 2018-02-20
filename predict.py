8da6143274931166ae3a50fb0495cead510c0d09
suyash0103 <suyashghuge@gmail.com>
2018-02-21 03:00:33 +0530

    Added function to send request to api

--- predict.py
+++ predict.py
@@ -12,3 +12,28 @@ target_date = datetime(2016, 5, 16)
 features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",
             "mintempm", "maxdewptm", "mindewptm", "maxpressurem", "minpressurem", "precipm"]
 DailySummary = namedtuple("DailySummary", features)  # named tuples are special types of tuples, backward compatible
+
+def extract_weather_data(url, api_key, target_date, days):
+    records = []
+    for _ in range(days):
+        request = BASE_URL.format(API_KEY, target_date.strftime('%Y%m%d'))
+        response = requests.get(request)
+        if response.status_code == 200:
+            data = response.json()['history']['dailysummary'][0]
+            records.append(DailySummary(
+                date=target_date,
+                meantempm=data['meantempm'],
+                meandewptm=data['meandewptm'],
+                meanpressurem=data['meanpressurem'],
+                maxhumidity=data['maxhumidity'],
+                minhumidity=data['minhumidity'],
+                maxtempm=data['maxtempm'],
+                mintempm=data['mintempm'],
+                maxdewptm=data['maxdewptm'],
+                mindewptm=data['mindewptm'],
+                maxpressurem=data['maxpressurem'],
+                minpressurem=data['minpressurem'],
+                precipm=data['precipm']))
+        time.sleep(6)
+        target_date += timedelta(days=1)
+    return records
