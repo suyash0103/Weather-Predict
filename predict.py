0f116350b7caf0cf8c93eae825109a83c6b934a7
suyash0103 <suyashghuge@gmail.com>
2018-02-21 13:23:37 +0530

    Test GET request

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import requests
 import matplotlib.pyplot as plt
 
 API_KEY = "ef504af496da69c7"
-BASE_URL = "http://api.wunderground.com/api/API_KEY/history_20180221/q/NE/Lincoln.json"
+BASE_URL = "http://api.wunderground.com/api/{}/history_{}/q/NE/Lincoln.json"
 
 target_date = datetime(2016, 5, 16)
 features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",
@@ -20,20 +20,23 @@ def extract_weather_data(url, api_key, target_date, days):
         response = requests.get(request)
         if response.status_code == 200:
             data = response.json()['history']['dailysummary'][0]   # extract history and dailysummary part of json
-            records.append(DailySummary(
-                date=target_date,
-                meantempm=data['meantempm'],
-                meandewptm=data['meandewptm'],
-                meanpressurem=data['meanpressurem'],
-                maxhumidity=data['maxhumidity'],
-                minhumidity=data['minhumidity'],
-                maxtempm=data['maxtempm'],
-                mintempm=data['mintempm'],
-                maxdewptm=data['maxdewptm'],
-                mindewptm=data['mindewptm'],
-                maxpressurem=data['maxpressurem'],
-                minpressurem=data['minpressurem'],
-                precipm=data['precipm']))
-        time.sleep(6)
-        target_date += timedelta(days=1)
+            print (data)
+        #     records.append(DailySummary(
+        #         date=target_date,
+        #         meantempm=data['meantempm'],
+        #         meandewptm=data['meandewptm'],
+        #         meanpressurem=data['meanpressurem'],
+        #         maxhumidity=data['maxhumidity'],
+        #         minhumidity=data['minhumidity'],
+        #         maxtempm=data['maxtempm'],
+        #         mintempm=data['mintempm'],
+        #         maxdewptm=data['maxdewptm'],
+        #         mindewptm=data['mindewptm'],
+        #         maxpressurem=data['maxpressurem'],
+        #         minpressurem=data['minpressurem'],
+        #         precipm=data['precipm']))
+        # time.sleep(6)
+        # target_date += timedelta(days=1)
     return records
+
+extract_weather_data(BASE_URL, API_KEY, target_date, 2)
