0acfff062337cf5973eee48ddd680e8022672786
suyash0103 <suyashghuge@gmail.com>
2018-02-21 13:31:48 +0530

    Send request to fetch data

--- predict.py
+++ predict.py
@@ -20,23 +20,23 @@ def extract_weather_data(url, api_key, target_date, days):
         response = requests.get(request)
         if response.status_code == 200:
             data = response.json()['history']['dailysummary'][0]   # extract history and dailysummary part of json
-            print (data)
-        #     records.append(DailySummary(
-        #         date=target_date,
-        #         meantempm=data['meantempm'],
-        #         meandewptm=data['meandewptm'],
-        #         meanpressurem=data['meanpressurem'],
-        #         maxhumidity=data['maxhumidity'],
-        #         minhumidity=data['minhumidity'],
-        #         maxtempm=data['maxtempm'],
-        #         mintempm=data['mintempm'],
-        #         maxdewptm=data['maxdewptm'],
-        #         mindewptm=data['mindewptm'],
-        #         maxpressurem=data['maxpressurem'],
-        #         minpressurem=data['minpressurem'],
-        #         precipm=data['precipm']))
-        # time.sleep(6)
-        # target_date += timedelta(days=1)
+            # print (data)
+            records.append(DailySummary(
+                date=target_date,
+                meantempm=data['meantempm'],
+                meandewptm=data['meandewptm'],
+                meanpressurem=data['meanpressurem'],
+                maxhumidity=data['maxhumidity'],
+                minhumidity=data['minhumidity'],
+                maxtempm=data['maxtempm'],
+                mintempm=data['mintempm'],
+                maxdewptm=data['maxdewptm'],
+                mindewptm=data['mindewptm'],
+                maxpressurem=data['maxpressurem'],
+                minpressurem=data['minpressurem'],
+                precipm=data['precipm']))
+        time.sleep(6)
+        target_date += timedelta(days=1)
     return records
 
-extract_weather_data(BASE_URL, API_KEY, target_date, 2)
+records = extract_weather_data(BASE_URL, API_KEY, target_date, 100)
